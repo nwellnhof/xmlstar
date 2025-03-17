cb6ac7f5ada7e7fcb873f9d05fad63ce234046d9
Nick Wellnhofer <wellnhofer@aevum.de>
2025-03-17 10:41:59 +0100

    Fix uninitialized variable in xml_edit.c

--- src/xml_edit.c
+++ src/xml_edit.c
@@ -336,7 +336,7 @@ edInsert(xmlDocPtr doc, xmlNodeSetPtr nodes, const char *val, const char *name,
 
     for (i = 0; i < nodes->nodeNr; i++)
     {
-        xmlNodePtr node;
+        xmlNodePtr node = NULL;
 
         if (nodes->nodeTab[i] == (void*) doc && mode != 0) {
             fprintf(stderr, "The document node cannot have siblings.\n");
